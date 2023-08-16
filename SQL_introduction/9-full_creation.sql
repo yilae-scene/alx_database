f91f996f87fb74f243795e0a5191ed6796815e38
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-16 20:59:02 +0300

    Create and Insert data

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,9 +1,11 @@
 -- full creation
+
 CREATE TABLE IF NOT EXISTS second_table
 (id INT NOT NULL AUTO_INCREMENT,
  name VARCHAR(256),
  score INT
 );
 
+-- insert data to table_2
 INSERT INTO second_table(name, score) 
 VALUES('Jhon', 10),('Alex', 3),('Bob', 14),('George', 8);
