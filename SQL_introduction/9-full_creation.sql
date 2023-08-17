407ecda3de6aaca2fa1a2c03fa8f048828827ac9
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-17 11:07:44 +0300

    djfkdjkf

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -7,9 +7,13 @@ CREATE TABLE IF NOT EXISTS second_table
 );
 
 -- insert data to table_2
-INSERT INTO second_table VALUES (1, "John", 10);
-INSERT INTO second_table VALUES (2, "Alex", 3);
-INSERT INTO second_table VALUES (3, "Bob", 14);
-INSERT INTO second_table VALUES (4, "George", 8);
+INSERT INTO second_table 
+VALUES (1, "John", 10);
+INSERT INTO second_table 
+VALUES (2, "Alex", 3);
+INSERT INTO second_table 
+VALUES (3, "Bob", 14);
+INSERT INTO second_table 
+VALUES (4, "George", 8);
 
 
