aa7f8b82dc736c692e7e14be897cd069500584b0
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-17 10:44:06 +0300

    Modify

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -7,18 +7,18 @@ CREATE TABLE IF NOT EXISTS second_table
 );
 
 -- insert data to table_2
-INSERT INTO second_table 
+INSERT INTO second_table (id, name, score)
 VALUES (1, 'Jhon', 10);
 
 -- insert another data
-INSERT INTO second_table
+INSERT INTO second_table (id, name, score)
 VALUES (2, 'Alex', 3);
 
 -- insert another data
-INSERT INTO second_table
+INSERT INTO second_table (id, name, score)
 VALUES (3, 'Bob', 14);
 
 -- insert another data
 
-INSERT INTO second_table
+INSERT INTO second_table (id, name, score)
 VALUES (4, 'George', 8);
