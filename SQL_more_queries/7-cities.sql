9684c3711fb691c33ee00ca426ac65b073588637
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-18 12:57:56 +0300

    Modify to add new column

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS cities
 (
     id INT AUTO_INCREMENT NOT NULL,
     states_id INT NOT NULL ,
+    name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (states_id) REFERENCES states (id)
 )
