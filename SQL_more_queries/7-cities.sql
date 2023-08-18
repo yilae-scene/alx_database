bc7d2e01d856e67ac2dbb33de6cbfde70cf55c38
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-18 12:55:35 +0300

    Create Table and add PK and FK

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- create database
+CREATE SCHEMA IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities
+(
+    id INT AUTO_INCREMENT NOT NULL,
+    states_id INT NOT NULL ,
+    PRIMARY KEY (id),
+    FOREIGN KEY (states_id) REFERENCES states (id)
+)
