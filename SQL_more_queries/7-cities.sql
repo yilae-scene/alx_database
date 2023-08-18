138b3cfd4a86678e0fc8b77370be95d5b64892d0
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-18 13:17:53 +0300

    Correct the FK

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,11 +1,13 @@
 -- create database
 CREATE SCHEMA IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities
 (
-	id INT UNIQUE AUTO_INCREMENT NOT NULL,
-	state_id INT NOT NULL,
-	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (state_id)
-	REFERENCES hbtn_0d_usa.states(id)
+    id INT UNIQUE AUTO_INCREMENT NOT NULL,
+    state_id INT NOT NULL ,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
+
+
