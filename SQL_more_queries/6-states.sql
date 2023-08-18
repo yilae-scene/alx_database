63785cb672f454064341884fab82496d445b0c64
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-18 12:26:13 +0300

    Create Database and Table

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,11 @@
+-- create a DATABASE
+CREATE SCHEMA IF NOT EXISTS hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states
+(
+    id INT NOT NULL AUTO_INCREMENT ,
+    name VARCHAR (256) NOT NULL,
+    PRIMARY KEY(id)
+)
+
+
