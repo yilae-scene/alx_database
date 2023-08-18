cd794365fec8e5f3881783e49a6a77d41f0b4f1f
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-18 13:13:36 +0300

    JGJHGJH

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,11 +1,11 @@
 -- create database
 CREATE SCHEMA IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
 (
-    id INT AUTO_INCREMENT NOT NULL,
-    state_id INT NOT NULL ,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (states_id) REFERENCES states (id)
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (state_id)
+	REFERENCES hbtn_0d_usa.states(id)
 );
