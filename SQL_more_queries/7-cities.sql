e40d4c6caa390c55308223affa151e503eefa5f1
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-18 13:00:06 +0300

    Modify column

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -4,7 +4,7 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities
 (
     id INT AUTO_INCREMENT NOT NULL,
-    states_id INT NOT NULL ,
+    state_id INT NOT NULL ,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (states_id) REFERENCES states (id)
