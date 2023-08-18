2e90cd88eb3956b606cfd709b2febfe2e3e73977
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-18 13:08:35 +0300

    Modify the table

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,12 +1,11 @@
 -- create database
 CREATE SCHEMA IF NOT EXISTS hbtn_0d_usa;
-
--- create table
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities
 (
     id INT AUTO_INCREMENT NOT NULL,
     state_id INT NOT NULL ,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (states_id) REFERENCES states (id)
-)
+);
